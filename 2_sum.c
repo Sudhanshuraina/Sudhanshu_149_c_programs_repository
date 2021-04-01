834460462a4e12b10789c01fd3e956df97f4a047
sudhanshu raina <2020a1r149@gmail.com>
2021-04-01 22:52:26 +0530

    sum of two numbers

---
+++ 2_sum.c
@@ -0,0 +1,10 @@
+//sudhanshu raina
+//A basic C Program to calculate sum of two numbers.				
+#include<stdio.h>
+int main(){
+	int number1, number2, sum;
+	scanf("%d %d", &number1, &number2); //Scanning values entered by user.
+	sum=number1+number2; //Processing the sum operation.
+	printf("Sum of 2 numbers= %d", sum); //Printing the output.
+	return 0;
+}
