d5e7371986494648266e08e0da64e146a53c65c8
sudhanshu raina <2020a1r149@gmail.com>
2021-04-02 00:29:39 +0530

    factorial recursion

--- 22_FibonacciRecursion.c
+++ 22_FibonacciRecursion.c
@@ -1,4 +1,38 @@
- //A C Program to print Fibonacci series using recursion.	 			© Ishav Verma 20/March/2021
+ //A C Program to print Fibonacci series using recursion.	 			//A C Program to print factorial using recursion.	 			© Ishav Verma 20/March/2021
+#include<stdio.h>
+	long int multiplyNumbers(int n);
+	int main() {
+    	int n;
+    	printf("Enter a positive integer: ");
+    	scanf("%d",&n);
+    	printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    	return 0;
+	}
+
+	long int multiplyNumbers(int n) {
+    	if (n>=1)
+        	return n*multiplyNumbers(n-1);
+    	else
+        	return 1;
+}
+© 2021 GitHub, Inc.//A C Program to print factorial using recursion.	 			© Ishav Verma 20/March/2021
+#include<stdio.h>
+	long int multiplyNumbers(int n);
+	int main() {
+    	int n;
+    	printf("Enter a positive integer: ");
+    	scanf("%d",&n);
+    	printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    	return 0;
+	}
+
+	long int multiplyNumbers(int n) {
+    	if (n>=1)
+        	return n*multiplyNumbers(n-1);
+    	else
+        	return 1;
+}
+© 2021 GitHub, Inc.
 #include<stdio.h>    
 int main(){    
 	int n1=0,n2=1,n3,i,number;    
