6caf22414de86c558c4af720506cde5ac0901588
sudhanshu raina <2020a1r149@gmail.com>
2021-04-02 02:55:55 +0530

    do while loop

---
+++ 41_dowhileloop.c
@@ -0,0 +1,16 @@
+//sudhanshu raina
+#include <stdio.h> //Pre-Process directive to include standard input output header file
+ 
+int main () { //Main function body starting
+
+   /* local variable definition */
+   int a = 10;
+
+   /* do loop execution */
+   do {
+      printf("value of a: %d\n", a);
+      a = a + 1;
+   }while( a < 20 );
+ 
+   return 0; 
+}
